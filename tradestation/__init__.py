3df930c30e0b0c8f097d75a3be7ff7d37f88e88f
Graeme Holliday <graeme.holliday@pm.me>
2024-09-25 11:11:47 -0500

    separate test and real pypi

--- tradestation/__init__.py
+++ tradestation/__init__.py
@@ -5,7 +5,7 @@ from .session import Session
 API_URL_V3 = 'https://api.tradestation.com/v3'
 API_URL_V2 = 'https://api.tradestation.com/v2'
 API_URL_SIM = 'https://sim-api.tradestation.com/v3'
-VERSION = '0.1'
+VERSION = '0.2'
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
